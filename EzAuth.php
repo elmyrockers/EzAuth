7fe8fde5d3bca78cf8aae0a6ceec893382c67083
elmyrockers <elmyrockers@gmail.com>
2024-06-14 04:37:24 +0800

    Modify EzAuth class

--- EzAuth.php
+++ EzAuth.php
@@ -90,9 +90,9 @@ class EzAuth
 			if ( !session_id() ) session_start();
 	}
 
-	public function register( $callback )
+	public function register( $callback = null )
 	{
-		
+		return true;
 	}
 }
 
@@ -100,64 +100,53 @@ class EzAuth
 
 
 
-# FOR TESTING PURPOSE
-$database = [
-	'dsn' => 'mysql:host=localhost;dbname=ezauth',
-	'username' => 'root',
-	'password' => ''
-];
-$email = [
-	'smtp_settings' => [
-		'debug' => true,
-		'host' => 'amazonaws.com',
-		'username' => 'username',
-		'password' => 'password',
-		'port' => '587',
-		'encryption' => 'starttls' // starttls / smtps
-	],
-	'from' => [ 'no-reply@yoursite.com', 'EzAuth' ],
-	'reply_to' => [ 'admin@yoursite.com', 'Admin' ],
-	'template_dir' => '/views/emails/',
-	'templates' => [
-		'email_verification' => [ 'subject', 'filename' ], // filename without extension (for html and plain text)
-		'reset_password' => [ 'subject', 'filename' ],
-	]
-];
-$message = [
-		// 'language' => [ 'en', '/optional/path/to/language/directory/' ],
-		'template' => [
-			'success' => '<div class="alert alert-success">{$message}</div>',
-			'error' => '<div class="alert alert-danger">{$message}</div>'
-		]
-];
+# FOR DEBUGGING PURPOSE
+// $database = [
+// 	'dsn' => 'mysql:host=localhost;dbname=ezauth',
+// 	'username' => 'root',
+// 	'password' => ''
+// ];
+// $email = [
+// 	'smtp_settings' => [
+// 		'debug' => true,
+// 		'host' => 'amazonaws.com',
+// 		'username' => 'username',
+// 		'password' => 'password',
+// 		'port' => '587',
+// 		'encryption' => 'starttls' // starttls / smtps
+// 	],
+// 	'from' => [ 'no-reply@yoursite.com', 'EzAuth' ],
+// 	'reply_to' => [ 'admin@yoursite.com', 'Admin' ],
+// 	'template_dir' => '/views/emails/',
+// 	'templates' => [
+// 		'email_verification' => [ 'subject', 'filename' ], // filename without extension (for html and plain text)
+// 		'reset_password' => [ 'subject', 'filename' ],
+// 	]
+// ];
+// $message = [
+// 		// 'language' => [ 'en', '/optional/path/to/language/directory/' ],
+// 		'template' => [
+// 			'success' => '<div class="alert alert-success">{$message}</div>',
+// 			'error' => '<div class="alert alert-danger">{$message}</div>'
+// 		]
+// ];
 
-const ROLE_USER = 0;
-const ROLE_ADMIN = 1;
-const ROLE_SUPERADMIN = 2;
-$auth = [
-		'id_fields' => [ 'email' ], // username / email / phone - follow table columns (its value must be unique)
-		'allowed_fields' => [ 'username','email', 'password', 'confirm_password' ],
+// const ROLE_USER = 0;
+// const ROLE_ADMIN = 1;
+// const ROLE_SUPERADMIN = 2;
+// $auth = [
+// 		'id_fields' => [ 'email' ], // username / email / phone - follow table columns (its value must be unique)
+// 		'allowed_fields' => [ 'username','email', 'password', 'confirm_password' ],
 
-		'logout_redirect' => '/auth/login/',
-		'member_area' => [
-			ROLE_USER => '/member/user/',
-			ROLE_ADMIN => '/member/admin/',
-			ROLE_SUPERADMIN => '/member/superadmin/'
-		],
-		'verify_email' => '',  // Specify page for email verification
-		'reset_password' => '' // Specify page for reset password
-];
-$ezauth = new EzAuth(compact( 'database', 'email', 'message', 'auth' ));
+// 		'logout_redirect' => '/auth/login/',
+// 		'member_area' => [
+// 			ROLE_USER => '/member/user/',
+// 			ROLE_ADMIN => '/member/admin/',
+// 			ROLE_SUPERADMIN => '/member/superadmin/'
+// 		],
+// 		'verify_email' => '',  // Specify page for email verification
+// 		'reset_password' => '' // Specify page for reset password
+// ];
+// $ezauth = new EzAuth(compact( 'database', 'email', 'message', 'auth' ));
 
-
-
-
-echo "<pre>";
-p( $ezauth );
-// P($_SERVER);
-
-
-function p( $value )
-{
-	print_r( $value );
-}
+// dump( $ezauth );
