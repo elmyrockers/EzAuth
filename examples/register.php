4162df15f87c41cbfde7a087f1a7911afeeb8e2c
elmyrockers <elmyrockers@gmail.com>
2024-06-15 16:04:30 +0800

    Update remove comments

--- examples/register.php
+++ examples/register.php
@@ -7,10 +7,4 @@ $_POST[ 'email' ] = 'elmyrockers@company.com';
 $_POST[ 'password' ] = '12345';
 $_POST[ 'confirm_password' ] = '12345';
 $ezauth->register();
-$flash = $ezauth->flashMessage();
-echo $flash;
-// $ezauth->verify_email( 'login.php' );
-// dump( $ezauth );
-
-// $secretKey = bin2hex( random_bytes( 32 ));
-// dump( $secretKey );
+echo $ezauth->flashMessage();
--- examples/verify_email.php
+++ examples/verify_email.php
@@ -2,10 +2,5 @@
 require_once 'config.php';
 
 
-// $ezauth->register();
-// $flash = $ezauth->flashMessage();
 $ezauth->verify_email( 'login.php' );
-// dump( $ezauth );
-
-// $secretKey = bin2hex( random_bytes( 32 ));
-// dump( $secretKey );
+echo $ezauth->flashMessage();
