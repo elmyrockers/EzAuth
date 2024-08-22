c0d0bbc058106d9080950e027a139b5d80860850
elmyrockers <elmyrockers@gmail.com>
2024-08-22 16:00:01 +0800

    Move html forms into forms directory

--- examples/forms/register_form.php
+++ examples/forms/register_form.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Register Form - EzAuth</title>
-		<link rel="stylesheet" href="./style.css">
+		<link rel="stylesheet" href="forms/style.css">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 	</head>
@@ -11,7 +11,7 @@
 		<div class="bg"></div>
 		<main class="form-signin">
 			<?=$flash ?>
-			<h1 class="h3">Register - EzAuth</h1>
+			<h1 class="h3">Register</h1>
 			<form method="post">
 				<div class="form-floating">
 					<input name="email" type="email" class="form-control" id="floatingInput" placeholder="Email Address" required value="test@gmail.com">
@@ -33,7 +33,7 @@
 				</div> -->
 				<button class="w-100 btn btn-lg" type="submit">Sign Up</button>
 			</form>
-			<p class="copyright">&copy; 2024</p>
+			<p class="copyright">&copy; 2024 EzAuth</p>
 		</main>
 	</body>
 </html>
