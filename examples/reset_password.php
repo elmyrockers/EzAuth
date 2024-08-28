6151805c29d8d639540f6e65e7702f3cf6cd48ca
elmyrockers <elmyrockers@gmail.com>
2024-08-29 02:53:25 +0800

    Add 3rd parameter for resetPassword method

--- examples/reset_password.php
+++ examples/reset_password.php
@@ -1,8 +1,9 @@
 <?php
 require_once 'config.php';
-list( $status, $flash, $csrfToken ) = $ezauth->resetPassword( 'login.php', $user );
+list( $status, $flash, $csrfToken ) = $ezauth->resetPassword( 'login.php', $user, $hasSent );
 
-
-
+if ( !$hasSent && $status === false ) {
+	header( "Location: login.php" ); exit;
+}
 
 include 'forms/reset_password_form.php';
