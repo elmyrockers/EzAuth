96b4582819435b14e69de4699c583f91060ef1c0
elmyrockers <elmyrockers@gmail.com>
2024-08-30 02:36:54 +0800

    Update example for register

--- examples/register.php
+++ examples/register.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'config.php';
+// $csrfToken = $ezauth->csrfToken();
+// $flash = $ezauth->flashMessage();
+// $status = $ezauth->register();
 list( $status, $flash, $csrfToken ) = $ezauth->register();
-
-
-
 
 include 'forms/register_form.php';
